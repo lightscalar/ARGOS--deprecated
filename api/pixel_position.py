3c68ca2066c74a17f8b949107c50c32c9101b7ec
Oliver <oliverhi@umich.edu>
2018-08-31 16:13:09 -0400

    pixel position

---
+++ api/pixel_position.py
@@ -0,0 +1,40 @@
+'''
+
+    Find the latitude, longitude position of any pixel in a georeferenced tif file.
+
+'''
+
+from osgeo import gdal, osr, ogr
+
+
+def transform_geodetic_to_latlon(ds, x, y):
+
+    source = osr.SpatialReference()
+    source.ImportFromWkt(ds.GetProjection())
+
+    target = osr.SpatialReference()
+    target.ImportFromEPSG(4326)
+
+    transform = osr.CoordinateTransformation(source, target)
+    return transform.TransformPoint(x, y)
+
+
+def pixel_to_coord(ds, col, row):
+
+    x_upper_left, xres, xskew, y_upper_left, yskew, yres = ds.GetGeoTransform()
+
+    nx = (xres * col) + (xskew * row) + x_upper_left
+    ny = (yskew * col) + (yres * row) + y_upper_left
+
+    return nx, ny
+
+
+if __name__ == '__main__':
+
+    ds = gdal.Open('MinerStreetSmall.tif')
+
+    x_upper_left, xres, xskew, y_upper_left, yskew, yres = ds.GetGeoTransform()
+    x, y = pixel_to_coord(ds, ds.RasterXSize, ds.RasterYSize)
+
+    print(f'0, 0 transform: {transform_geodetic_to_latlon(ds, x_upper_left, y_upper_left)}')
+    print(f'With pixel offset: {transform_geodetic_to_latlon(ds, x, y)}')
